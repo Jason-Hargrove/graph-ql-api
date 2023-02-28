ecebd26bfe34c15f0407cafc03f78d65e9ca5079
Jason Hargrove <rijaha@gmail.com>
2023-02-28 07:57:10 -0600

    set up resolvers

--- index.js
+++ index.js
@@ -1,24 +1,34 @@
 const { ApolloServer, gql } = require("apollo-server");
 
-const typeDefs = gql`
-type Query {
-    sessions:[Session]
-}
-type Session {
-    id: ID!,
-    title: String!,
-    description: String,
-    startsAt: String,
-    endsAt: String,
-    room: String,
-    day: String,
-    format: String,
-    track: String,
-    level: String
-}
-`
+const sessions = require("./data/sessions.json");
 
-const server = new ApolloServer({typeDefs});
+const typeDefs = gql`
+  type Query {
+    sessions: [Session]
+  }
+  type Session {
+    id: ID!
+    title: String!
+    description: String
+    startsAt: String
+    endsAt: String
+    room: String
+    day: String
+    format: String
+    track: String
+    level: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    sessions: () => {
+      return sessions;
+    },
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
 
 server.listen({ port: process.env.PORT || 4000 }).then((url) => {
   console.log(`graphQL running at ${url}`);
