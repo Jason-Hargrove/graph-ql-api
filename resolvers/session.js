c834395b3a3ad6da9e434a90fe8bb19dfcaba457
Jason Hargrove <rijaha@gmail.com>
2023-03-04 11:09:54 -0600

    throwing errors

--- resolvers/session.js
+++ resolvers/session.js
@@ -1,11 +1,18 @@
-const _ = require('lodash');
+const _ = require('lodash')
+const { ApolloError } = require('apollo-server')
 
 module.exports = {
   speakers: async (session, args, { dataSources }, info) => {
-    const speakers = await dataSources.speakerAPI.getSpeakers();
-    const returns = speakers.filter((speaker) => {
-      return _.filter(session.speakers, { id: speaker.id }).length > 0;
-    });
-    return returns;
+    const speakers = await dataSources.speakerAPI.getSpeakers()
+    try {
+      const returns = speakers.filter((speaker) => {
+        return _.filter(session.speakers, { id: speaker.id }).length > 0
+      })
+      return returns
+    } catch (error) {
+      return new ApolloError('Unable to get speakers', 'SPEAKERAPIERROR', {
+        token: 'UNIQUETOKEN',
+      })
+    }
   },
-};
+}
