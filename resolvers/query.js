045026b57bc1fe6668576929b4504942fb789d71
Jason Hargrove <rijaha@gmail.com>
2023-03-05 12:31:26 -0600

    throw errors

--- resolvers/query.js
+++ resolvers/query.js
@@ -1,14 +1,22 @@
 module.exports = {
   sessions: (parent, args, { dataSources }, info) => {
-    return dataSources.sessionAPI.getSessions(args);
+    return dataSources.sessionAPI.getSessions(args)
   },
   sessionById: (parent, { id }, { dataSources }, info) => {
-    return dataSources.sessionAPI.getSessionById(id);
+    try {
+      return dataSources.sessionAPI.getSessionById(id)
+    } catch (error) {
+      return {
+        code: 'ERROR',
+        message: 'An error occured',
+        token: '1234rhrfethfew',
+      }
+    }
   },
   speakers: (parent, args, { dataSources }, info) => {
-    return dataSources.speakerAPI.getSpeakers();
+    return dataSources.speakerAPI.getSpeakers()
   },
   speakerById: (parent, { id }, { dataSources }, info) => {
-    return dataSources.speakerAPI.getSpeakerById(id);
+    return dataSources.speakerAPI.getSpeakerById(id)
   },
-};
+}
